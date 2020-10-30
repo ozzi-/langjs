fed5f90a9445e6c717239b0d4f0cf1651f2acc3e
ozzi- <oz@oz-web.com>
2020-10-30 11:49:35 +0100

    Add files via upload

--- language.js
+++ language.js
@@ -1,78 +1,76 @@
 // ************
 // TRANSLATIONS
 // ************
-var de = new Array();
-de['surname'] = 'Nachnamen';
-de['name'] = 'Name';
 
-var fr = new Array();
-fr['surname'] = 'Nom de famille';
-fr['name'] = 'Nom';
+var langjs = {
+
+	languages: {},
 
 
+	// ****
+	// CODE
+	// ****
 
-// ****
-// CODE
-// ****
+	currentLanguage : "",
 
-initLang();
+	initLang: function (languagesToInit){
+		for (var i = 0; i < languagesToInit.length; i++) {
+			this.languages[languagesToInit[i]] = {};
+		}
 
-var currentLanguage;
-var currentLanguageDictionary;
-var defaultLanguage="de";
+		var lang = this.readCookie("lang");
+		if(lang==null || lang === undefined || lang == ""){
+			this.setLanguage(Object.keys(languages)[0]);
+		}else{
+			this.setLanguage(lang);
+		}
+	},
 
-function initLang(){
-	var lang = readCookie("lang");
-	if(lang==null){
-		setLanguage(defaultLanguage);
-	}else{
-		setLanguage(lang);
+	getCurrentLanguage: function (){
+		return this.currentLanguage;
+	},
+
+	addTranslation: function (key,lang,val){
+		this.languages[lang][key] = val;
+	},
+
+	setLanguage: function (lang,reload){
+		this.currentLanguage=lang;
+		if(this.languages[lang] == undefined){
+			alert("Language "+lang+" failed to load.");
+		}
+		document.cookie = "lang="+lang+"; SameSite=Lax";
+		if(reload!==undefined && reload){
+	 		location.reload();
+		}
+	},
+
+	getString: function (key){
+		if(this.languages[this.currentLanguage][key] !== undefined ){
+			return this.languages[this.currentLanguage][key];	
+		}
+		return "{missing translation}"
+	},
+
+	fillString: function (key){
+		if(this.languages[this.currentLanguage][key] !== undefined ){
+			document.write(this.languages[this.currentLanguage][key]);	
+		}else{
+			document.write("{missing translation}");		
+		}
+	},
+
+	// *******
+	// HELPERS
+	// *******
+	readCookie: function (name) {
+		var nameEQ = name + "=";
+		var ca = document.cookie.split(';');
+		for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+		}
+		return null;
 	}
-
-
-}
-
-function setLanguage(lang,reload){
-	currentLanguage=lang;
-	if(lang=="de"){
-		currentLanguageDictionary = de;
-	}else if(lang=="fr"){
-		currentLanguageDictionary = fr;
-	}else{
-		alert("Language "+lang+" failed to load.");
-	}
-	document.cookie = "lang="+lang+"; SameSite=Lax";
-	if(reload!==undefined && reload){
- 		location.reload();
-	}
-}
-
-function getString(key){
-	if(currentLanguageDictionary[key] !== undefined ){
-		return currentLanguageDictionary[key];	
-	}
-	return "{missing translation}"
-}
-
-function fillString(key){
-	if(currentLanguageDictionary[key] !== undefined ){
-		document.write(currentLanguageDictionary[key]);	
-	}else{
-		document.write("{missing translation}");		
-	}
-}
-
-
-// *******
-// HELPERS
-// *******
-function readCookie(name) {
-	var nameEQ = name + "=";
-	var ca = document.cookie.split(';');
-	for(var i=0;i < ca.length;i++) {
-	var c = ca[i];
-	while (c.charAt(0)==' ') c = c.substring(1,c.length);
-	if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-	}
-	return null;
-}
+};
